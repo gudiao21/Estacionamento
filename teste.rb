7d4b82f3b9f0a35ffe3317ba073e7fb34794196b
gudiao21 <lucianodantas53@gmail.com>
2023-03-12 10:50:50 -0300

    Committing teste2.rb file

--- teste.rb
+++ teste.rb
@@ -16,22 +16,4 @@
 # outra_classe = OutraClasse.new
 # outra_classe.chamar_metodo_da_outra_classe
 
-def self.cadastrar_entrada
-    #debugger
-    system 'clear'
-    puts "\n|----- Voce escolheu a opção: (1)CADASTRAR ENTRADA DO VEÍCULO -----|\n\n"
-    @novo_veiculo = {}
-    print "Digite a placa do veiculo: "
-    @novo_veiculo[:placa] = gets.to_s.strip.chomp
-    print "Digite o nome do veículo: "
-    @novo_veiculo[:nome_veiculo] = gets.to_s.strip.chomp
-    print "Digite o nome do proprietário do veículo: "
-    @novo_veiculo[:dono_do_veiculo] = gets.to_s.chomp
-    print "Digite a hora de entrada do veículo: "
-    @novo_veiculo[:hora_entrada] = Time.parse(gets.chomp)
-    @@veiculos[@novo_veiculo[:placa]]= @novo_veiculo
-    puts "+==========================================+"
-    puts "|      VEÍCULO CADASTRADO COM SUCESSO.     |"
-    puts "+==========================================+"
-    ControleVeiculos.volta_menu
-end
+jhkjhjkhkkjhkhjkhkjhhjkhkjhjkhkjhjkjjkhjkjkhjkhjkhjkhhjhjkhhjkjkjkkkkjkjkjhjkjkkjhkjhkjkhkjhjhjkhjkkklkhhkklhkhklkjjhjhkkjkjhhkhlhkhkhklhkkhhkjlhklkjlkjljlçjjçjljlkjljljlçjljljljljljlçlçjljljljljljljljljljjljljllkjljljljljçjlkjjkjjkljljjljlkjljljljljlçjlkjçuoiuiojlkjn,n,mvbcxczcxasdqwerrytttyuiyiuyhjgjdgdfsadxczcxcbcetrtyerdfsdfkklllklklklklklklklklklkkljkkjljlkklkllklklljllljkljkllkjljljljlklkjjçljklkljklkljkljkljllkjjjljkjkjlkjjklkljklkljkljlkçkjklçjioiuoiiolkjkluioklilkhjkhkjhhjkjkhjkhhhjkhkhhjkkhjkkjhkjjkhkjluyuhjkbnmjhkjhgffgasd\xzxcxfgtrwersdazcxbbnm,klhyyiujkhhgfhhjghjghjghjhjhjjhghjghjghjjhjhgjhgggggjhgjhgjggjkgjhgjgjhggggjhgjggjghggjkghgghjkghjghgjghjghjgjhghghjgghhjkjghjkjkgjkgjghjhkgjhgghghjgggggjhgjhjhjghjggggggjhghjkghjkgjkgkghghjgjkghjggjhgjhgjhghjghjgjhghjkghjkggghhghghjgjhghjghjhjgjjhgjhghjgjhgjhgjggghjgjhghjkghjgjhghjgjhkjhghjghjgjhgjhgjhgjhghjjhggkjkjgjgjgjgjkgjhghjghjghjghgghjgjjjgjhgjhjhghjghjghjghjhjgjhgjhjhjhgjhjhjhjhjhgjhjhgjhgkhjhkgjhgjhgjhgjhgjhjhgjhghjghjghjghjghjhjhghghjhjgjhhjgjjgjjgjhgjhghjgjhghjgjhgjhgjhjhgjhgjhgjhghjghjghjgjhgjhjhjhghjjhghjghjhgjgkgjhghjhhjhjgjhghjghjgjhkgghjgjhhjghjghjgjhhjghghjgjhhjgjhhjhjghjghjhjgjhgjhhjghjggjkghjghjghjghjhjgjhjhghjghjgggjhgjhggjgjhjgghjkjhgjhgjhjhgjhhjghjgjhhjghjgjhggjhgjhgjhhjghjjhgjhgjhgjhghjghjgjhhjghjghjghjghjghjhjghjghjgjhghjghjjhgjhjhjhjhjhjhhjhjghjghjgjhghjhjhjghjghjhjhjgjhghjghjghjghjghjhjghjhjhghghghjhjhjhjhjhjghjghjhjghghjhjhjhjhjghjghjghjhjhjgghhgjkhjghjghjhjghjghjghjkjghjhjghjhjghjhjghjghjghjhjhjghjghgjhjghjgjhghjghghjhjghjgjgjjhgjhjhjhhjghjghjhjjkhghjghjhjgjhjhhjhjgjhjhhjghjghjhjghjghjhjghjjhghjg
