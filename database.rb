065876f3365d9ef04c59e87b491ec87af1f2f9e7
gudiao21 <lucianodantas53@gmail.com>
2023-04-12 05:32:34 -0300

    Committing estacionamento.rb file on remote main

--- database.rb
+++ database.rb
@@ -1,20 +1,25 @@
 require 'pg'
+require_relative 'estacionamento'
 
 class Database
+
   def self.delete_veiculo(placa)
+    system 'clear'
     begin
       conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira', host: 'localhost', port: '5432')
-      #conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira')
       conn.exec_params("DELETE FROM estacionamento.controle_veiculos WHERE placa = '#{placa}'")
     rescue PG::Error => e
       puts e.message
     ensure
       conn.close unless conn.nil?
     end
+    print "=========================================================="
     print "Veículo de placa #{placa} foi excluído com sucesso!"
+    print "=========================================================="
   end
 
   def self.relatorio_veiculos
+    system 'clear'
     begin
       conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira', host: 'localhost', port: '5432')
       result = conn.exec("SELECT * FROM estacionamento.controle_veiculos;")
@@ -28,10 +33,12 @@ class Database
     ensure
       conn.close unless conn.nil?
     end
+    ControleVeiculos.volta_menu
   end
 
   def self.inserir_veiculo(placa, nome_veiculo, dono_do_veiculo, hora_entrada)
-      begin
+    system 'clear'
+    begin
         conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira', host: 'localhost', port: '5432')
         conn.exec_params("INSERT INTO estacionamento.controle_veiculos (placa, nome_veiculo, dono_do_veiculo, hora_entrada) VALUES ($1, $2, $3, $4)", [placa, nome_veiculo, dono_do_veiculo, hora_entrada])
       rescue PG::Error => e
@@ -42,6 +49,7 @@ class Database
   end
 
   def self.edit_veiculo(placa, nome_veiculo, dono_do_veiculo, hora_entrada)
+    system 'clear'
     begin
       conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira', host: 'localhost', port: '5432')
       conn.exec_params("UPDATE estacionamento.controle_veiculos SET nome_veiculo=$1, dono_do_veiculo=$2, hora_entrada=$3 WHERE placa=$4", [nome_veiculo, dono_do_veiculo, hora_entrada, placa])
@@ -53,15 +61,30 @@ class Database
   end
 
   def self.registrar_saida(placa, hora_saida)
-    # print "Digite a placa do veículo que deseja editar: "
-    # placa = gets.chomp
-    # print "Digite a hora da saída do veículo: "
-    # hora_saída_str = gets.to_s.chomp
-    # hora_saida = Time.parse(hora_entrada_str).strftime("%Y-%m-%d %H:%M:%S")
-
+    system 'clear'
     connection = PG.connect(host: 'localhost', dbname: 'estacionamento', user: 'postgres', password: 'Joacira')
-    connection.prepare('insert_saida', 'INSERT INTO estacionamento.controle_veiculos (placa, hora_saida) VALUES ($1, $2)')
-    result = connection.exec_prepared('insert_saida', [placa, hora_saida])
+    connection.prepare('update_saida', 'UPDATE estacionamento.controle_veiculos SET hora_saida = $1 WHERE placa = $2')
+    result = connection.exec_prepared('update_saida', [hora_saida, placa])
+    connection.close
+  end
+
+  def self.buscar_veiculo(placa)
+    connection = PG.connect(host: 'localhost', dbname: 'estacionamento', user: 'postgres', password: 'Joacira')
+    connection.prepare('select_veiculo', 'SELECT placa, nome_veiculo, dono_do_veiculo, hora_entrada, hora_saida FROM estacionamento.controle_veiculos WHERE placa = $1')
+    result = connection.exec_prepared('select_veiculo', [placa])
+
+    if result.ntuples == 0
+      puts "Veículo não encontrado."
+    else
+      result.each do |row|
+        puts "Placa: #{row['placa']}"
+        puts "Nome do veículo: #{row['nome_veiculo']}"
+        puts "Dono do veículo: #{row['dono_do_veiculo']}"
+        puts "Hora de entrada: #{row['hora_entrada']}"
+        puts "Hora de saída: #{row['hora_saida'] || 'N/A'}"
+      end
+    end
+
     connection.close
   end
 
--- estacionamento.rb
+++ estacionamento.rb
@@ -6,37 +6,11 @@ require 'term/ansicolor'
 require 'pastel'
 require_relative 'database'
 
-# conn = PG.connect(
-#   dbname: "estacionamento",
-#   user: "postgres",
-#   password: "Joacira",
-#   host: "localhost",
-#   port: "5432"
-# )
-
-
 #debugger
 class Veiculo
 
-  def mostrar_entrada_saida(placa, nome_veiculo, dono_do_veiculo, hora_entrada, hora_saida) #Método para cada veículo.
-    system 'clear'
-    #debugger
-    puts "Placa do carro é #{placa}."
-    puts "O nome do carro é #{nome_veiculo}."
-    puts "O nome do proprietário é #{dono_do_veiculo}."
-    puts "A hora de entrada foi #{hora_entrada}."
-    if hora_saida == nil
-      puts "A hora da saída ainda não foi registrada!"
-      puts "=================================================="
-    else
-      puts "A hora de saída foi #{hora_saida}."
-      puts "=================================================="
-    end
-    ControleVeiculos.volta_menu
-  end
-  #dados[:total_a_pagar] ? ("%.2f" % dados[:total_a_pagar]) : ""
-
   def self.editar_veiculo #Receberá novos valores para o veículo.
+    system 'clear'
     print "Digite a placa do veículo que deseja editar: "
     placa = gets.chomp
     print "Digite o novo nome do veículo: "
@@ -59,17 +33,7 @@ class Veiculo
 end
 
 class ControleVeiculos #Sempre no padrão de codificação "Pascal Case".
-  SAIR_DO_SISTEMA = 6
-
-  @@veiculos = {}
-
-  def self.objeto_json
-    puts "-------|Confirmação de registração em JSON|-------"
-    json_veiculos = JSON.generate(@@veiculos)
-    File.write('veiculos.json', json_veiculos)
-    puts "Veículo(s) registrado(s):\n#{json_veiculos}"
-    puts "--------------------------------------------------"
-  end
+  SAIR_DO_SISTEMA = 8
 
   def initialize
     @veiculos = {}
@@ -86,42 +50,14 @@ class ControleVeiculos #Sempre no padrão de codificação "Pascal Case".
     puts "Digite (1) para cadastrar entrada do veículo."
     puts "Digite (2) para cadastrar saída do veículo."
     puts "Digite (3) para buscar veículo por placa."
-    puts "Digite (4) para mostrar movimentação do dia."
-    puts "Digite (5) para gerenciar o BD do PostgreSQL."
-    puts "Digite (6) para sair."
+    puts "Digite (4) para relatório do dia."
+    puts "Digite (5) para deletar um registro no postgre."
+    puts "Digite (6) editar um registro de um veículo"
+    puts "Digite (7) para sair."
     puts "====================================================="
     ControleVeiculos.captura_item_menu
   end
 
-  def self.menu_postgre
-    system 'clear'
-    puts "\nO que deseja fazer?\n\n"
-    puts "Digite (1) para deletar um registro no postgre."
-    puts "Digite (2) para relatório corrente de veículos."
-    puts "Digite (3) para editar um registro de um veículo."
-    ControleVeiculos.captura_item_menu_postgre
-    ControleVeiculos.volta_menu
-  end
-
-  def self.captura_item_menu_postgre
-    opcao = gets.to_i
-    case opcao
-    when 1
-      system 'clear'
-      print "Digite a placa do veículo a ser deletado: "
-      placa = gets.chomp
-      #chamada do método para deletar o veículo
-      Database.delete_veiculo(placa)
-    when 2
-      system 'clear'
-      Database.relatorio_veiculos
-      ControleVeiculos.volta_menu
-    when 3
-      system 'clear'
-      Veiculo.editar_veiculo
-    end
-
-  end
 
   def self.captura_item_menu
     opcao = gets.to_i
@@ -133,9 +69,16 @@ class ControleVeiculos #Sempre no padrão de codificação "Pascal Case".
     when 3
       ControleVeiculos.buscar_veiculo
     when 4
-      ControleVeiculos.relatorio
+      Database.relatorio_veiculos
     when 5
-      ControleVeiculos.menu_postgre
+      Database.delete_veiculo
+    when 6
+      print "Digite a placa do veículo a ser deletado: "
+      placa = gets.chomp
+      #chamada do método para deletar o veículo
+      Database.delete_veiculo(placa)
+    when 7
+      Veiculo.editar_veiculo
     when SAIR_DO_SISTEMA
       SAIR_DO_SISTEMA
     else
@@ -212,131 +155,35 @@ class ControleVeiculos #Sempre no padrão de codificação "Pascal Case".
     system 'clear'
     puts "\n|----- Voce escolheu a opção: (2)CADASTRAR SAÍDA DO VEÍCULO -----|\n\n"
 
-    debugger
+    #debugger
     print "Digite a placa do veiculo: "
-    placa = gets.to_s.strip
-    #debugger
+    placa = ""
+    while placa.empty?
+      placa = gets.to_s.strip
+    end
+
     print "Digite a hora de saída do veículo (formato: HH:MM): "
-    hora_saida_str = gets.chomp
-    hora_saida = Time.parse(hora_saida_str).strftime("%Y-%m-%d %H:%M:%S")
-    #debugger
-    if @@veiculos.key?(placa) && @@veiculos[placa][:hora_saida].nil?
-      #debugger
-      @novo_veiculo = @@veiculos[placa]
-      hora_saida = nil
-
-      print "Digite a hora de saída do veículo (formato: HH:MM): "
+    hora_saida_str = ""
+    while hora_saida_str.empty?
       hora_saida_str = gets.chomp
-      hora_saida = Time.parse(hora_saida_str).strftime("%Y-%m-%d %H:%M:%S")
+    end
+    hora_saida = Time.parse(hora_saida_str).strftime("%Y-%m-%d %H:%M:%S")
 
-      begin
-        @novo_veiculo[:hora_saida] = DateTime.parse(hora_saida).to_time
 
-            rescue ArgumentError
-        print "Hora de saída inválida, tente novamente: "
-        retry
-      end
 
      Database.registrar_saida(placa, hora_saida)
 
-      # conn = PG.connect(dbname: 'estacionamento', user: 'postgres', password: 'Joacira', host: 'localhost', port: 5432)
-      # conn.prepare('insert_statement', 'INSERT INTO estacionamento (placa, nome_veiculo, dono_do_veiculo, hora_entrada) VALUES ($1, $2, $3, $4)')
-      # conn.prepare('select_statement', 'SELECT * FROM estacionamento')
-      # result = conn.exec_prepared('select_statement')
-      #
-      # puts "\n+---|confirmação de cadastro no banco de dados do postgresql|---+"
-      # result.each do |row|
-      #   puts "Placa: #{row['placa']}, Nome do veículo: #{row['nome_veiculo']}, Dono do veículo: #{row['dono_do_veiculo']}, Hora de entrada: #{row['hora_entrada']}"
-      # end
-      # puts "+-----------------------------------------------------------------+"
 
-      #debugger
-      @@veiculos[placa][:hora_saida] = hora_saida #Corrigido 07/03/23, estava @@veiculos[:placa] ...
-      puts "+==========================================+"
-      puts "|       SAÍDA CADASTRADA COM SUCESSO.      |"
-      puts "+==========================================+"
-      ControleVeiculos.objeto_json
-      #debugger
-      ControleVeiculos.calculo(@@veiculos[placa][:placa], @@veiculos[placa][:hora_entrada], hora_saida)
-
-    elsif @@veiculos.key?(placa) && @@veiculos[placa][:hora_saida]
-      puts "Hora de saída já registrada para essa placa!"
-      ControleVeiculos.volta_menu
-
-    else
-      puts "\n\n"
-      puts "+-----------------------------------------------------------------+"
-      puts "|        A placa (#{placa}) ainda não foi cadastrada.             |"
-      puts "| Deseja ir para a opção: (1)CADASTRAR ENTRADA DE VEÍCULO? (S/N)  |"
-      puts "|                                                                 |"
-      puts "+-----------------------------------------------------------------+"
-      escolha = gets.to_s.upcase.strip
-
-      case escolha
-      when "S"
-        ControleVeiculos.cadastrar_entrada
-      when "N"
-        ControleVeiculos.menu
-      else
-        system 'clear'
-        puts "(S) ou (N) não escolhida, por isso te redirecionaremos para a opção do MENU!"
-        ControleVeiculos.pausa
-        ControleVeiculos.menu
-      end
-
-    end
   end
 
   def self.buscar_veiculo
-    system 'clear'
-    puts "\n|----- Voce escolheu a opção: (3)BUSCAR VEÍCULO POR PLACA -----|\n\n"
-    #debugger
-    print "Digite a placa do veículo: "
-    placa = gets.strip
-    if @@veiculos.key?(placa) #Se "placa" já estiver sendo usada como chave em @@veiculos, o que indica que um veículo com essa placa já foi cadastrado.
-      veiculo = @@veiculos[placa]
-      puts "Veículo de placa #{veiculo[:placa]} encontrado!"
-      Veiculo.new.mostrar_entrada_saida(veiculo[:placa], veiculo[:nome_veiculo], veiculo[:dono_do_veiculo], veiculo[:hora_entrada], veiculo[:hora_saida])
-    else
-      puts "Veículo de placa #{veiculo} não encontrado!"
-    end
-    ControleVeiculos.volta_menu
-  end
+     system 'clear'
+     puts "\n|----- Voce escolheu a opção: (3)BUSCAR VEÍCULO POR PLACA -----|\n\n"
+  #   #debugger
+     print "Digite a placa do veículo: "
+     placa = gets.strip
+     Database.buscar_veiculo(placa)
 
-  def self.relatorio
-    #debugger
-    system 'clear'
-    # Define as larguras máximas de cada coluna
-    placa_width = 5
-    nome_veiculo_width = 20
-    dono_do_veiculo_width = 20
-    hora_entrada_width = 32
-    hora_saida_width = 32
-    total_a_pagar_width = 14
-    subtotais_width = 50
-
-    # Cria a string de formatação
-    format_string = "%-#{placa_width}s  %-#{nome_veiculo_width}s  %-#{dono_do_veiculo_width}s  %-#{hora_entrada_width}s  %-#{hora_saida_width}s  %-#{total_a_pagar_width}s  %-#{subtotais_width}s\n"
-
-    # Imprime o cabeçalho
-    puts "====================================================================================================================================================="
-    puts format_string % ["Placa", "Nome do Veículo", "Dono do Veículo", "Hora de Entrada", "Hora de Saída", "Total a Pagar", "Subtotais"]
-    puts "====================================================================================================================================================="
-    #debugger
-    ControleVeiculos.calcular_subtotal
-    # Percorre todos os veículos e os imprime formatados:
-    @@veiculos.each do |placa, dados|
-      # Substitui valores nulos ou vazios por uma string vazia:
-      placa = placa.nil? ? "" : placa
-      nome_veiculo = dados[:nome_veiculo].nil? ? "" : dados[:nome_veiculo]
-      dono_do_veiculo = dados[:dono_do_veiculo].nil? ? "" : dados[:dono_do_veiculo]
-      hora_entrada = dados[:hora_entrada].nil? ? "" : dados[:hora_entrada]
-      hora_saida = dados[:hora_saida].nil? ? "" : dados[:hora_saida]
-      total_a_pagar = dados[:total_a_pagar].nil? ? "" : "%.2f" % dados[:total_a_pagar]
-      subtotal = dados[:subtotal].nil? ? "" : "%.2f" % dados[:subtotal]
-      printf(format_string, placa, dados[:nome_veiculo], dados[:dono_do_veiculo], dados[:hora_entrada], dados[:hora_saida], dados[:total_a_pagar] ? ("%.2f" % dados[:total_a_pagar]) : 0, dados[:subtotal] ? ("%.2f" % dados[:subtotal]) : 0)
-    end
-    ControleVeiculos.volta_menu
   end
 
   def self.calculo(placa, hora_entrada, hora_saida)
